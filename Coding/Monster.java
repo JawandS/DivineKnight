2e32ebca9eb0fa8e74a4ad65e70318133b24a0eb
Lin Lynn Tao <lynntao@Lins-Air.fios-router.home>
2020-06-12 11:16:15 -0400

    Updated full pseudocode for Monster Class

--- Coding/Monster.java
+++ Coding/Monster.java
@@ -1,4 +1,49 @@
 public class Monster
 {
+   //pseudocode
+   /* int armor;
+      int item;
+      */
+}
 
+class Attacks
+{
+   //pseudocode
+   /* int itemNumber;
+      int damage;
+      int range;
+      */
+}
+class Value
+{
+   class Gear
+   {
+   //pseudocode
+   /* 
+      int numitems;
+      double probability;
+      int value;
+       */
+   }
+   class Gold
+   {
+   //pseudocode
+   /* 
+      int gold;
+      */
+   }
+}
+class Health
+{
+   //pseudocode
+   /* 
+      int health;
+      */
+}
+class Boss
+{
+   //pseudocode
+   /* 
+      boolean boss;
+      */
 }
