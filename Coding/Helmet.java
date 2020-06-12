436595baca99843318fea53994c0c23e1fbd3223
Jawand <jawandsingh@gmail.com>
2020-06-12 12:21:56 -0400

    Edit coding class, create abstract for gear

---
+++ Coding/Helmet.java
@@ -0,0 +1,15 @@
+public class Helmet{
+
+//vars
+int hn;
+int hv;
+int hd;
+
+//constructor
+public Helmet(int hn, int hv, double hd){
+hn = hn;
+hv = hv;
+hd = hd;
+}
+
+}
---
+++ Coding/Knight.java
@@ -0,0 +1,19 @@
+
+
+public class Knight{
+//variables
+
+//item number - int
+Helmet h;
+
+//constructor 
+
+public Knight(int hn, int hv, double hd){
+
+h = new Helmet(hn, hv, hd);
+
+}
+
+
+}
+
